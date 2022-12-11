7a2a74c219f66fbba7816af14b907cf93ed5146d
LukeG007 <luke@gilsinger.net>
2022-12-11 12:55:47 -0500

    initial commit

---
+++ test_client.py
@@ -0,0 +1,14 @@
+import announcement_client
+import time
+
+@announcement_client.announcement_callback
+def announcement(message, sender):
+    print('New Announcement: "{}" from {}'.format(message, sender))
+
+announcement_client.run_client('test')
+
+x = 1
+while True:
+    announcement_client.announce('This is a test message that is sent every 5 seconds. Count: {}'.format(x))
+    x += 1
+    time.sleep(5)
